c81b3250392a7e5237e1508164e9e698b5ab2b6a
Vicu <vicugaideic@gmail.com>
2022-03-07 18:32:34 +0300

    focus effect and fix select options

---
+++ src/components/CustomOption.vue
@@ -0,0 +1,60 @@
+<script>
+export default {
+  name: "CustomOption",
+
+  props: {
+    option: {
+      required: false,
+      type: Number,
+      default: 1,
+    },
+  },
+
+  data: () => ({
+    attrs: {
+      color: "darken-2",
+      required: true,
+      class: "input-option",
+      disabled: true,
+    },
+  }),
+};
+</script>
+
+<template>
+  <div>
+    <div v-if="option === 1">
+      <v-text-field v-bind="attrs" label="Short answer"> </v-text-field>
+    </div>
+    <div v-if="option === 2">
+      <v-text-field label="Detailed answer" v-bind="attrs"> </v-text-field>
+    </div>
+    <div v-if="option === 6">
+      <v-text-field
+        label="Day, month, year"
+        v-bind="attrs"
+        append-icon="mdi-calendar-range"
+      >
+      </v-text-field>
+    </div>
+    <div v-if="option === 7">
+      <v-text-field
+        label="Time"
+        class="input-date"
+        append-icon="mdi-clock-time-five-outline"
+        v-bind="attrs"
+      >
+      </v-text-field>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.input-date {
+  max-width: 180px;
+}
+
+.input-option {
+  max-width: 500px;
+}
+</style>
--- src/views/NewForm.vue
+++ src/views/NewForm.vue
@@ -1,18 +1,23 @@
 <script>
+import CustomOption from "@/components/CustomOption";
+
 export default {
   name: "NewForm",
 
+  components: { CustomOption },
+
   data: () => ({
-    question: true,
-    selectedCard: null,
     close: false,
+    selectedCard: 0,
     questions: [
       {
         id: 0,
         form: [{ formId: 0, value: "Option 1" }],
+        input: "Question",
+        selectedOption: 1,
+        focused: true,
       },
     ],
-    selectedOption: 1,
     selectOptions: [
       { value: 1, text: " Text (string)", icon: "mdi-text-short" },
       { value: 2, text: " Text (paragraph)", icon: "mdi-text-long" },
@@ -35,45 +40,54 @@ export default {
       { value: 7, text: "Time", icon: "mdi-clock-time-five-outline" },
     ],
   }),
-  computed: {
-    isList() {
-      const listElements = [3, 4, 5];
 
-      return listElements.includes(this.selectedOption);
-    },
-  },
   methods: {
     focus(name) {
       this.selectedCard = name;
     },
-    deleteItem(id) {
-      this.questions.splice(id, 1);
+    deleteItem(index) {
+      this.questions.splice(index, 1);
     },
     addItem() {
       this.questions.push({
-        id: Math.random() * 100000,
+        id: new Date().getTime(),
         form: [
           {
-            formId: Math.random() * 100000,
+            formId: new Date().getTime(),
             value: `Option 1`,
           },
         ],
+        input: "Question",
+        selectedOption: 1,
       });
     },
     copyItem(item) {
       this.questions.push({
-        id: Math.random() * 100000,
+        id: new Date().getTime(),
         form: JSON.parse(JSON.stringify(item.form)),
+        input: item.input,
+        selectedOption: item.selectedOption,
+        focused: true,
+      });
+    },
+    focusItem() {
+      this.questions.forEach((item, index) => {
+        item.focused = index !== this.selectedCard;
       });
     },
     addOption(item) {
       item.form.push({
-        formId: Math.random() * 100000,
+        formId: new Date().getTime(),
         value: `Option ${item.form.length + 1}`,
       });
     },
     deleteOption(item, index) {
       item.form.splice(index, 1);
+    },
+    checkboxOptions(item) {
+      const listElements = [3, 4, 5];
+
+      return listElements.includes(item.selectedOption);
     },
   },
 };
@@ -104,33 +118,52 @@ export default {
         </v-card>
         <div v-for="(item, index) of questions" :key="item.id">
           <v-card
-            :ripple="false"
-            @focus="focus('second')"
-            :class="{ 'border-left': selectedCard === 'second' }"
+            :class="{ 'border-left': selectedCard === index }"
             class="mb-5 question mx-auto pa-5"
             max-width="700"
             outlined
-            @click="question = false"
+            @mousedown="focus(index)"
+            @click="focusItem()"
+            v-ripple="false"
           >
-            <v-card-text v-if="question">
-              <div class="text-h4">Question</div>
-              <v-text-field
-                color="purple darken-2"
-                value="Option 1"
-                required
-              ></v-text-field>
+            <v-card-text v-if="item.focused && checkboxOptions(item)">
+              <div class="text-h4">{{ item.input }}</div>
+              <div v-for="(forms, formsIndex) of item.form" :key="forms.formId">
+                <div class="d-flex justify-space-between align-center">
+                  <v-icon v-if="item.selectedOption === 3" class="icon"
+                    >mdi-radiobox-blank
+                  </v-icon>
+                  <v-icon v-if="item.selectedOption === 4" class="icon"
+                    >mdi-crop-square
+                  </v-icon>
+                  <div v-if="item.selectedOption === 5" class="icon">
+                    {{ formsIndex + 1 }}.
+                  </div>
+                  <v-text-field
+                    color="purple darken-2"
+                    required
+                    v-model="forms.value"
+                    class="ml-7 input-option"
+                    readonly
+                  >
+                  </v-text-field>
+                </div>
+              </div>
             </v-card-text>
+            <custom-option v-else-if="item.focused"></custom-option>
             <v-card-text v-else>
               <div class="d-flex justify-space-between">
                 <v-text-field
                   placeholder="Question"
+                  v-model="item.input"
                   class="mr-5"
                   filled
                   required
                 ></v-text-field>
                 <v-select
-                  v-model="selectedOption"
+                  v-model="item.selectedOption"
                   :items="selectOptions"
+                  @click="checkboxOptions(item)"
                   label="Select Operation"
                   :menu-props="{ bottom: true, offsetY: true }"
                   outlined
@@ -145,20 +178,20 @@ export default {
                   </template>
                 </v-select>
               </div>
-              <div v-if="isList">
+              <div v-if="checkboxOptions(item)">
                 <div
                   v-for="(forms, formsIndex) of item.form"
                   :key="forms.formId"
                 >
                   <v-hover v-slot="{ hover }">
                     <div class="d-flex justify-space-between align-center">
-                      <v-icon v-if="selectedOption === 3" class="icon"
+                      <v-icon v-if="item.selectedOption === 3" class="icon"
                         >mdi-radiobox-blank
                       </v-icon>
-                      <v-icon v-if="selectedOption === 4" class="icon"
+                      <v-icon v-if="item.selectedOption === 4" class="icon"
                         >mdi-crop-square
                       </v-icon>
-                      <div v-if="selectedOption === 5" class="icon">
+                      <div v-if="item.selectedOption === 5" class="icon">
                         {{ formsIndex + 1 }}.
                       </div>
                       <v-text-field
@@ -186,55 +219,14 @@ export default {
                   </v-btn>
                 </div>
               </div>
-              <div v-if="selectedOption === 1">
-                <v-text-field
-                  color=" darken-2"
-                  required
-                  label="Short answer"
-                  class="input-option"
-                >
-                </v-text-field>
-              </div>
-              <div v-if="selectedOption === 2">
-                <v-text-field
-                  color="darken-2"
-                  required
-                  label="Detailed answer"
-                  class="input-option"
-                >
-                </v-text-field>
-              </div>
-              <div v-if="selectedOption === 6">
-                <v-text-field
-                  color="darken-2"
-                  required
-                  label="Day, month, year"
-                  class="input-date"
-                  disabled
-                  append-icon="mdi-calendar-range"
-                >
-                </v-text-field>
-              </div>
-              <div v-if="selectedOption === 7">
-                <v-text-field
-                  color="darken-2"
-                  required
-                  label="Time"
-                  class="input-date"
-                  append-icon="mdi-clock-time-five-outline"
-                  disabled
-                >
-                </v-text-field>
-              </div>
+              <custom-option :option="item.selectedOption"></custom-option>
               <div class="icons d-flex align-center justify-lg-end mt-5">
-                <v-btn icon fab>
-                  <v-icon @click="copyItem(item)"> mdi-content-copy </v-icon>
+                <v-btn @click="copyItem(item)" icon fab>
+                  <v-icon> mdi-content-copy </v-icon>
                 </v-btn>
 
-                <v-btn icon fab>
-                  <v-icon @click="deleteItem(index)" class="mx-5">
-                    mdi-delete-empty
-                  </v-icon>
+                <v-btn @click="deleteItem(index)" icon fab>
+                  <v-icon class="mx-5"> mdi-delete-empty </v-icon>
                 </v-btn>
                 <v-switch
                   label="Required question"
@@ -264,18 +256,14 @@ export default {
 .body {
   height: 100%;
   background-color: rgb(240, 235, 248);
+  position: relative;
 }
-.input-option {
-  max-width: 500px;
-}
+
 .icons {
   padding-top: 15px;
   border-top: 1px solid #e1e2e5;
 }
 .icon {
   position: absolute;
-}
-.input-date {
-  max-width: 180px;
 }
 </style>
